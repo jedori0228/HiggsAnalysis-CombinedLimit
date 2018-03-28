5db56d8e6d2bbcb6116ae02cfe2e51562c854101
Jae Sung Kim <jae.sung.kim@cern.ch>
2018-03-28 10:18:16 +0900

    py updated

--- data/2016_HNDiLepton/MakeCycleForMixingLimitVBF.py
+++ data/2016_HNDiLepton/MakeCycleForMixingLimitVBF.py
@@ -1,7 +1,6 @@
 import os
 
 channels = ["MuMu", "ElEl", "MuEl"]
-channels = ["ElEl", "MuEl"]
 Bins = ["Bin1", "Bin2", "Combined"]
 
 masses = [300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1700, 2000]

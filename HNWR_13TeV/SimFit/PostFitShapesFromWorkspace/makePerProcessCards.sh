41bba67db25a38781638252652f8dbd75dc0311c
Jae Sung Kim <jae.sung.kim@cern.ch>
2020-10-28 12:54:01 +0900

    PostFitShapesFromWorkspace scripts added

---
+++ HNWR_13TeV/SimFit/PostFitShapesFromWorkspace/makePerProcessCards.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+cd CardForEachYear
+
+for dirName in DYJets_MG_HT_Reweighted_Reshaped Others TTLX_powheg
+#for dirName in DYJets_MG_HT_Reweighted_Reshaped Others TT_TW NonPrompt
+do
+
+  cd ${dirName}
+
+  for channel in EE MuMu
+  do
+    for region in Resolved Boosted
+    do
+      combineCards.py Run2016_${channel}_${region}_SR=2016_card_${channel}_${region}_SR_WR5000_N3000.txt Run2017_${channel}_${region}_SR=2017_card_${channel}_${region}_SR_WR5000_N3000.txt Run2018_${channel}_${region}_SR=2018_card_${channel}_${region}_SR_WR5000_N3000.txt &> YearCombined_card_${channel}_${region}_SR_WR5000_N3000.txt
+    done
+  done
+
+  text2workspace.py YearCombined_card_EE_Boosted_SR_WR5000_N3000.txt --X-allow-no-signal
+  text2workspace.py YearCombined_card_EE_Resolved_SR_WR5000_N3000.txt --X-allow-no-signal
+  text2workspace.py YearCombined_card_MuMu_Boosted_SR_WR5000_N3000.txt --X-allow-no-signal
+  text2workspace.py YearCombined_card_MuMu_Resolved_SR_WR5000_N3000.txt --X-allow-no-signal
+  cd -
+done
+
+cd ..
+
+for sample in DYJets_MG_HT_Reweighted_Reshaped Others TTLX_powheg
+#for sample in DYJets_MG_HT_Reweighted_Reshaped Others TT_TW NonPrompt
+do
+  mv CardForEachYear/${sample}/YearCombined_card_EE_Boosted_SR_WR5000_N3000.root ${sample}__YearCombined_card_EE_Boosted_SR_WR5000_N3000.root
+  mv CardForEachYear/${sample}/YearCombined_card_EE_Boosted_SR_WR5000_N3000.txt ${sample}__YearCombined_card_EE_Boosted_SR_WR5000_N3000.txt
+  mv CardForEachYear/${sample}/YearCombined_card_EE_Resolved_SR_WR5000_N3000.root ${sample}__YearCombined_card_EE_Resolved_SR_WR5000_N3000.root
+  mv CardForEachYear/${sample}/YearCombined_card_EE_Resolved_SR_WR5000_N3000.txt ${sample}__YearCombined_card_EE_Resolved_SR_WR5000_N3000.txt
+
+  mv CardForEachYear/${sample}/YearCombined_card_MuMu_Boosted_SR_WR5000_N3000.root ${sample}__YearCombined_card_MuMu_Boosted_SR_WR5000_N3000.root
+  mv CardForEachYear/${sample}/YearCombined_card_MuMu_Boosted_SR_WR5000_N3000.txt ${sample}__YearCombined_card_MuMu_Boosted_SR_WR5000_N3000.txt
+  mv CardForEachYear/${sample}/YearCombined_card_MuMu_Resolved_SR_WR5000_N3000.root ${sample}__YearCombined_card_MuMu_Resolved_SR_WR5000_N3000.root
+  mv CardForEachYear/${sample}/YearCombined_card_MuMu_Resolved_SR_WR5000_N3000.txt ${sample}__YearCombined_card_MuMu_Resolved_SR_WR5000_N3000.txt
+done 
+
+cd ..
---
+++ HNWR_13TeV/SimFit/PostFitShapesFromWorkspace/printPerProcess.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import os
+
+Samples = [
+'DYJets_MG_HT_Reweighted_Reshaped__',
+'Others__',
+'TTLX_powheg__',
+#'TT_TW__',
+#'NonPrompt__',
+]
+
+cards = [
+'EE_Boosted_SR_WR5000_N3000',
+'EE_Resolved_SR_WR5000_N3000',
+'MuMu_Boosted_SR_WR5000_N3000',
+'MuMu_Resolved_SR_WR5000_N3000',
+]
+
+for card in cards:
+
+  channel = card.split('_')[0]
+
+  for Sample in Samples:
+
+    cardName = Sample+'YearCombined_card_'+card
+    cmd = 'PostFitShapesFromWorkspace -w %s.root -d %s.txt -o output/hists_%s.root --postfit --sampling -f fitDiagnostics_YearCombined_card_CRAdded_%s_Combined_WR5000_N3000.root:fit_b --total-shapes --samples 2000'%(cardName,cardName,cardName,channel)
+    print cmd
